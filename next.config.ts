67796b78488554d7411b5d8db5727b9dc7e253c0
Jaswanth <jaswanthbjk@gmail.com>
2025-07-24 14:10:51 +0000

    How can I host it on github

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type {NextConfig} from 'next';
 
+const repo = 'your-github-repo-name';
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,6 +16,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
